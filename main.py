f0c241b24350d04ead84560e825468a7359eadb3
Allan Elleuch <allan.elleuch@gmail.com>
2017-11-27 01:48:38 -0500

    update main pour tester matrix

--- main.py
+++ main.py
@@ -21,6 +21,17 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+
+
+from sklearn import metrics
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import make_scorer
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import f1_score
+
+
 hydropathy = { 'A': 1.8,'R':-4.5,'N':-3.5,'D':-3.5,'C': 2.5,
         'Q':-3.5,'E':-3.5,'G':-0.4,'H':-3.2,'I': 4.5,
         'L': 3.8,'K':-3.9,'M': 1.9,'F': 2.8,'P':-1.6,
@@ -148,7 +159,7 @@ def getFeatures(data,train=True):
         dataHydropathy.append(hydropathySequence)
         datatension.append(tensionSequence)
 
-    print("min : " + str(len(min(listSeq))))
+    # print("min : " + str(len(min(listSeq))))
 
     window_size = 16
     half_window = (window_size-1)//2
@@ -252,7 +263,7 @@ def getFeatures(data,train=True):
     # print(tfidf_matrix)
     # print(vectorizer.get_feature_names())
     # print(X)
-    return final
+    return tfidf_matrix
 
 
 # generator = SeqIO.parse("C:/Users/escroc/Documents/projectBioInformatique/fasta20171101.seq", "fasta")
@@ -277,8 +288,8 @@ def read(file,number=-1):
 dataSetSize = 100
 dataSetSizeTraining = dataSetSize//2 if dataSetSize >=0 else -1
 
-dataBrutePositive = read("Supp-A-prunned.txt",3000)
-dataBruteNegative = read("Supp-B-prunned.txt",3000 )
+dataBrutePositive = read("Supp-A-prunned.txt",-1 ) # size 73260
+dataBruteNegative = read("Supp-B-prunned.txt",-1 ) # size 72960 #TOTAL 146 220
 # dataBrutePositive=dataBrutePositive[:len(dataBrutePositive)/2]
 # dataBruteNegative=dataBruteNegative[:len(dataBruteNegative)/2]
 from random import shuffle
@@ -309,7 +320,7 @@ print("y2 : " + str(len(y2)))
 # features2 = getFeatures(dataBruteNegative) # SUppose que données sont des pairs bout à bout
 
 
-print(len(y2))
+# print(len(y2))
 clf = RandomForestClassifier(max_depth=100, random_state=0,n_jobs=-1,n_estimators=50,max_features=None,oob_score = True)
 # param_grid = {
 #     # 'n_estimators': [200, 700],
@@ -322,8 +333,9 @@ featuresTest = getFeatures(dataMerge)
 from scipy import *
 print("info1")
 print("features shapes :" + str(featuresTest.shape))
+
+
 # print("y size : " +  )
-clf.fit(featuresTest,y+y2 )
 
 # merge = hstack((featuresTest,y+y2))
 # np.random.shuffle(merge)
@@ -347,11 +359,80 @@ clf.fit(featuresTest,y+y2 )
 # merge = vstack((features,features2))
 # print(merge)
 
+
+# ENTRAINEMENT:  + VISUALISATION SI POSSIBLE
+# param_grid = {
+#     'n_estimators': [50,200, 700],
+#     'max_features': ['auto', 'sqrt', 'log2']
+# }
+
+param_grid = {
+    'n_estimators': [50]
+}
+# scoring = {'AUC': 'roc_auc', 'Accuracy': make_scorer(accuracy_score)}
+
+scoring = {'Accuracy': make_scorer(accuracy_score),'Recall': make_scorer(recall_score),'f1':make_scorer(f1_score)}
+
+# CV_rfc = GridSearchCV(estimator=clf,
+#                   param_grid=param_grid,
+#                   scoring=scoring, cv=5, refit='AUC') #, refit='AUC'
+# CV_rfc = GridSearchCV(estimator=clf,
+#                   param_grid=param_grid,
+#                   scoring=scoring, cv=5, refit='f1')
+# CV_rfc.fit(featuresTest, y+y2)
+# print(CV_rfc.best_params_)
+# results = CV_rfc.cv_results_
+# print(results)
+# import matplotlib.pyplot as plt
+
+#
+# plt.figure(figsize=(13, 13))
+# plt.title("GridSearchCV evaluating using multiple scorers simultaneously",
+#           fontsize=16)
+#
+# plt.xlabel("min_samples_split")
+# plt.ylabel("Score")
+# plt.grid()
+#
+# ax = plt.axes()
+# ax.set_xlim(0, 402)
+# ax.set_ylim(0.73, 1)
+#
+# # Get the regular numpy array from the MaskedArray
+# print(results)
+# X_axis = np.array(results['n_estimators'].data, dtype=float)
+#
+# for scorer, color in zip(sorted(scoring), ['g', 'k']):
+#     for sample, style in (('train', '--'), ('test', '-')):
+#         sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+#         sample_score_std = results['std_%s_%s' % (sample, scorer)]
+#         ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+#                         sample_score_mean + sample_score_std,
+#                         alpha=0.1 if sample == 'test' else 0, color=color)
+#         ax.plot(X_axis, sample_score_mean, style, color=color,
+#                 alpha=1 if sample == 'test' else 0.7,
+#                 label="%s (%s)" % (scorer, sample))
+#
+#     best_index = np.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
+#     best_score = results['mean_test_%s' % scorer][best_index]
+#
+#     # Plot a dotted vertical line at the best score for that scorer marked by x
+#     ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+#             linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+#
+#     # Annotate the best score for that scorer
+#     ax.annotate("%0.2f" % best_score,
+#                 (X_axis[best_index], best_score + 0.005))
+#
+# plt.legend(loc="best")
+# plt.grid('off')
+# plt.show()
+
 # clf.fit(features,y )
 # clf.fit(features2,y2 )
 # print(features2)
 
-dataBruteTest = read("Supp-E-prunned.txt",-1 )
+dataBruteTest = read("Supp-E-prunned.txt",100 )
 features3= getFeatures(dataBruteTest,train=False) # SUppose que données sont des pairs bout à bout
 
 
@@ -368,16 +449,24 @@ else :
     yTest=[1 for x in range(nbFeatures)]
 
 print(len(yTest))
-# prediction=-1
 from sklearn.model_selection import cross_val_score
 
-prediction2 = clf.predict(features3)
-prediction = clf.score(features3,yTest)
-print("prediction " + str(prediction))
-# print(mean(prediction))
-from sklearn import metrics
-from sklearn.model_selection import cross_validate
-print(np.array(yTest))
+# clf.fit(featuresTest,y+y2 )
+# prediction2 = clf.predict(features3)
+# prediction = clf.score(features3,yTest)
+# print("prediction" + str(prediction))
+
+
+
+
+# print(np.array(yTest))
+# print(prediction2)
+
+
+
+
+
+
 # print(prediction)
 # print(metrics.matthews_corrcoef(np.array(yTest), prediction2))
 # scores2 = cross_val_score(clf, features3, np.array(yTest))
@@ -388,11 +477,20 @@ print(np.array(yTest))
 # for mean, std, params in zip(means, stds, clf.cv_results_['params']):
 #     print("%0.3f (+/-%0.03f) for %r"
 #           % (mean, std * 2, params))
-scores2 = cross_validate(clf,X= features3,y= np.array(yTest), return_train_score=False,cv=5,scoring='precision_macro' )
 
-from sklearn.metrics import classification_report
-print("prediction" + str(prediction))
-print(classification_report(np.array(yTest), prediction2))
+
+
+# scores2 = cross_validate(clf,X= features3,y=np.array(yTest),cv=2,scoring=scoring ) # return_train_score=False,
+scores2 = cross_validate(clf,X= featuresTest,y=np.array(y+y2),cv=5,scoring=scoring) # return_train_score=False,
+print(scores2)
+
+print("Cross validate in 5 k mean ")
+print("mean fit time " + str( mean( scores2['fit_time']))   )
+print("mean accuracy  " + str( mean( scores2['test_Accuracy']))   )
+print("mean test_Recall  " + str( mean( scores2['test_Recall']))   )
+print("mean test_f1  " + str( mean( scores2['test_f1']))   )
+
+
 # pscore = metrics.accuracy_score(np.array(yTest), prediction)
 # score = metrics.f1_score(yTest, prediction)
 # print(score)
@@ -402,10 +500,8 @@ print(classification_report(np.array(yTest), prediction2))
 # print(yTest)
 # print("Accuracy: %0.2f (+/- %0.2f)" % (scores2.mean(), scores2.std() * 2))
 # print(len(scores2))
-print(scores2)
 
 
-# import matplotlib.pyplot as plt
 #résultat full data set  0.537194473964 de computation 320.96s] avec 2 features par prot => seq
 #0.529224229543 with just mean
 #0.515409139214 with max
